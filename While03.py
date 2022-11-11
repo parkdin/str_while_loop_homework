7ad604b66a8bf6ae0d9e731165757b2ef17785c8
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-11 18:35:18 +0900

    done

--- While03.py
+++ While03.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    import string
+    punc = string.punctuation
+    i = 0
+    ans = 0
+    while i < len(s):
+        if s[i] in punc:
+            ans += 1
+        i += 1
+    return ans
