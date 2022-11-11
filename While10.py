bcbb3db5f887de2a4f15618d70e07617fa898593
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-11 18:30:05 +0900

    done

--- While10.py
+++ While10.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    sum = 0
+    while i < len(s):
+        if int(s[i]) % 2 != 0:
+            sum += int(s[i])
+        i += 1
+    return sum
