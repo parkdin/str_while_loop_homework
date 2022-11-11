c3053a2270bd97c7735ff7ff8ca7939cf29078c8
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-11 18:03:38 +0900

    done

--- While04.py
+++ While04.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    ans = 0
+    while i < len(s):
+        if s[i].isupper():
+            ans += 1
+        i += 1
+    return ans
+print(main("CodeschOolUz"))
